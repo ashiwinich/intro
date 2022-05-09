128da2b84895d3fee6985891c428acb50abaaeb7
ashiwinich <105148280+ashiwinich@users.noreply.github.com>
2022-05-09 21:52:13 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,6 @@
+start_date = "Jan 1, 2020"
+end_date = "Mar 31, 2020"
+idx = pd.date_range(start_date,end_date)
+print(f"Length of index is {len(idx)}")
+items_sold = pd.Series(np.random.randint(25,50,size=len(idx)),index=idx)
+items_sold.plot()
